a8e1cd4bf4efd58fd8043afa23ba26484ac96124
afdezayl <mekin777@gmail.com>
2020-06-10 17:31:11 +0200

    fixed test, jest requires node version 12

--- apps/front/src/app/app.component.spec.ts
+++ apps/front/src/app/app.component.spec.ts
@@ -16,18 +16,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to front!'
+    expect(compiled.querySelector('h1').textContent.toLocaleUpperCase()).toContain(
+      'Chat&Call'.toLocaleUpperCase()
     );
   });
 });
