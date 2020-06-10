3713d0857af0b07efff7f2ca02ad837f5155b104
afdezayl <mekin777@gmail.com>
2020-06-10 17:12:16 +0200

    front and back apps. update dependencies

---
+++ apps/front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-and-call-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Chat&Call';
+}
