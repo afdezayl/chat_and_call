2a4f13ad95abefb690c44f0cc345fe501cd9593a
afdezayl <mekin777@gmail.com>
2020-06-10 21:21:23 +0200

    Config reader

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevTestsGateway } from './dev-tests.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService, DevTestsGateway],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { GatewayExplorerModule } from '@chat-and-call/utils/feature-gateway-explorer';
 import { SocketClusterAdapter } from '@chat-and-call/socketcluster/adapter';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
   app.useWebSocketAdapter(
     new SocketClusterAdapter(app, {
       path: '/socket',
-      authKey: 'askfjsaf', //config.get('JSON_WEBTOKEN_KEY'),
+      authKey: config.get('JSON_WEBTOKEN_KEY'),
       socketChannelLimit: 1000,
       origins: '*:*',
       authDefaultExpiry: 3600,
